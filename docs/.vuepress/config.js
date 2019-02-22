a91337436884522526edb6b57dcddac66dabb353
Jill <18687018392@163.com>
2019-02-22 18:07:42 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,32 +31,10 @@ module.exports = {
         ]
       },
     ],
-    sidebar: {
-      '/A/': [
-        '',
-        'one',
-        'two'
-      ],
-      '/B/': [
-        '',
-        'three',
-        'four'
-      ],
-      '/': [
-        '',
-        'appid',
-        'install',
-        'start',
-        'guide'
-      ],
-      '/install': 'install',
-      '/start': 'start',
-      '/guide': 'guide',
-      'appid': 'appid',
-      '/article1': {
+    sidebar: [{
         title: '文章一',
         path: '/docs/article1/',
-        collapsable: false,
+        collapsable: true,
         sidebarDepth: 1,
         children: [
           ['/article1/part1-1', "part1-1"],
@@ -65,17 +43,63 @@ module.exports = {
           ['/article1/part1-4', "part1-4"]
         ]
       },
-      'article2': {
+      {
         title: '文章二',
         path: '/docs/article2/',
-        collapsable: false,
+        collapsable: true,
         sidebarDepth: 1,
         children: [
           ['/article2/part2-1', 'part2-1'],
           ['/article2/part2-2', 'part2-2']
         ]
       }
-    },
+
+    ]
+    // sidebar: {
+    //   '/A/': [
+    //     '',
+    //     'one',
+    //     'two'
+    //   ],
+    //   '/B/': [
+    //     '',
+    //     'three',
+    //     'four'
+    //   ],
+    //   '/': [
+    //     '',
+    //     'appid',
+    //     'install',
+    //     'start',
+    //     'guide'
+    //   ],
+    //   '/install': 'install',
+    //   '/start': 'start',
+    //   '/guide': 'guide',
+    //   'appid': 'appid',
+    //   '/article1': {
+    //     title: '文章一',
+    //     path: '/docs/article1/',
+    //     collapsable: false,
+    //     sidebarDepth: 1,
+    //     children: [
+    //       ['/article1/part1-1', "part1-1"],
+    //       ['/article1/part1-2', "part1-2"],
+    //       ['/article1/part1-3', "part1-3"],
+    //       ['/article1/part1-4', "part1-4"]
+    //     ]
+    //   },
+    //   'article2': {
+    //     title: '文章二',
+    //     path: '/docs/article2/',
+    //     collapsable: false,
+    //     sidebarDepth: 1,
+    //     children: [
+    //       ['/article2/part2-1', 'part2-1'],
+    //       ['/article2/part2-2', 'part2-2']
+    //     ]
+    //   }
+    // },
 
     // "install", "start", "guide", "appid",
 
